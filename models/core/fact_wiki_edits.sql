61b3deef2d5addab7191a571e341e6a9554649df
albert.ningwa@gmail.com <albert.ningwa@gmail.com>
2026-03-27 18:13:37 +0000

    Added Bot and Human labels to the data

--- models/core/fact_wiki_edits.sql
+++ models/core/fact_wiki_edits.sql
@@ -5,17 +5,18 @@
       "data_type": "date",
       "granularity": "day"
     },
-    cluster_by = ["wiki_language", "is_bot"]
+    cluster_by = ["wiki_language", "user_type"]
 ) }}
 
 select
     edit_id,
-    event_date,
-    event_timestamp,
-    wiki_language,
+    wiki_language,         
+    language_code,
     page_title,
     user_name,
-    is_bot,
+    user_type,        
+    event_type,
     domain_name,
-    event_type
+    event_timestamp,
+    event_date
 from {{ ref('stg_wikimedia_edits') }}
