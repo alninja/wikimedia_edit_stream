88bf18f502d44964d6e1667300a53c024f4927f8
alninja <55853000+alninja@users.noreply.github.com>
2026-03-27 21:31:41 +0300

    Update stg_wikimedia_edits.sql

--- models/staging/stg_wikimedia_edits.sql
+++ models/staging/stg_wikimedia_edits.sql
@@ -1,27 +1,39 @@
 {{ config(materialized='view') }}
 
-with raw_data as 
-(
-  select *
-  from {{ source('staging','raw_stream') }}
-  where id is not null 
+with raw_data as (
+    select *
+    from {{ source('staging','raw_stream') }}
+    /* Filter out nulls early to keep the data clean */
+    where id is not null 
 )
+
 select
     -- Identifiers
     cast(id as string) as edit_id,
     cast(wiki as string) as wiki_language,
     
+    -- Extracting Language Code (e.g., 'enwiki' -> 'en')
+    left(cast(wiki as string), 2) as language_code,
+
     -- Edit Details
     cast(title as string) as page_title,
     cast(user as string) as user_name,
     cast(bot as boolean) as is_bot,
+    
+    -- Transforming Boolean to Label for the Dashboard
+    CASE 
+        WHEN cast(bot as boolean) = true THEN 'Bot'
+        WHEN cast(bot as boolean) = false THEN 'Human'
+        ELSE 'Unknown'
+    END AS user_type,
+
     cast(type as string) as event_type,
     cast(domain as string) as domain_name,
     
-    -- Timestamps (Converting the Integer timestamp to a BigQuery Timestamp)
+    -- Timestamps
     timestamp_seconds(cast(timestamp as int64)) as event_timestamp,
     
-    -- Extract the Date for Partitioning in the next step
+    -- Date for Partitioning
     date(timestamp_seconds(cast(timestamp as int64))) as event_date
 
 from raw_data
